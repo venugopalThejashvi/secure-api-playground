2f97fed84cf44c0de6734513892a25f415e9c768
thejashvivenugopal <venugopal.t@northeastern.edu>
2025-11-10 18:03:20 -0500

    kafka topic configured

---
+++ src/main/java/com/care/predict/kafka/kafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package com.care.predict.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class kafkaTopicConfig {
+
+    @Bean
+    public NewTopic registrationTopic(){
+        return TopicBuilder.name(Topic.userRegistered)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
