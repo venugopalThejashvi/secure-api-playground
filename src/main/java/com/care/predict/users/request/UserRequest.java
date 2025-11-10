e7f20c25264aa75ee6fdffa2656460dd99eb7967
thejashvivenugopal <venugopal.t@northeastern.edu>
2025-11-10 18:05:46 -0500

    userService modified to send messages to email consumer

--- src/main/java/com/care/predict/users/request/UserRequest.java
+++ src/main/java/com/care/predict/users/request/UserRequest.java
@@ -1,5 +1,6 @@
 package com.care.predict.users.request;
 
+import com.care.predict.validator.username.EmailCheck;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequest {
+    @EmailCheck
     private String username;
     private String password;
     private String role;
